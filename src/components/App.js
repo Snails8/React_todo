77c84ad192d18ba2e1cc3b4f851686a7764cb261
Suzuki Kaito <tanichi.u.n.20@gmail.com>
2021-03-24 07:40:03 +0900

    form 作成

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component } from 'react';
+import Form from './Form';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>React To Do</h1>
+        <Form />
       </div>
     );
   } 
