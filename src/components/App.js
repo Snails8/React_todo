e2c836f2a56e5eb7823d2ca6a35fdf4c9ab6757b
Suzuki Kaito <tanichi.u.n.20@gmail.com>
2021-03-25 08:33:13 +0900

    データの保存 作成

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,24 @@ import Form from './Form';
 import List from './List';
 
 export default class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          todo: []
+      };
+      this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  // データ保存
+  handleAdd(e){
+    console.log(e.target.title.value);
+    e.preventDefault();
+  }
   render() {
     return (
       <div className="App">
-        <h1>React To Do</h1>
-        <Form />
+        <h1>React To Do App</h1>
+        <Form handleAdd={this.handleAdd}/>
 
         <List />
       </div>
