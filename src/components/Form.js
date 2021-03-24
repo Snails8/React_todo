3d71d483d4df67aa10fd4b479e69b8e0226da9da
Suzuki Kaito <tanichi.u.n.20@gmail.com>
2021-03-25 08:41:35 +0900

    データ送信：props作成

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 const Form = (props) => (
-    <form className="simple-form">
+    <form className="simple-form" onSubmit={props.handleAdd}>
         <div className="form-group">
             <input type="text" name="title" id="title" className="form-control" placeholder="ココ"/>
             <label for="title">form here</label>
